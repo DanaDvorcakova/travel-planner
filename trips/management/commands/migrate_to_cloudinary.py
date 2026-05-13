03988b76deffc48d8d81f71e106ac862a505db1d
DanaDvorcakova <danadvorcakova@gmail.com>
2026-05-13 15:34:37 +0100

    Add command to upload existing images to Cloudinary

--- trips/management/commands/migrate_to_cloudinary.py
+++ trips/management/commands/migrate_to_cloudinary.py
@@ -1,21 +1,20 @@
 from django.core.management.base import BaseCommand
-from trips.models import Profile, Trip, SavedPlace, Place
+from trips.models import Trip, SavedPlace
+from django.core.files import File
+import os
 
 class Command(BaseCommand):
-    help = "Upload all images from local storage to Cloudinary"
+    help = 'Upload existing images to Cloudinary'
 
     def handle(self, *args, **kwargs):
-        models_with_images = [Profile, Trip, SavedPlace, Place]
+        for obj in Trip.objects.all():
+            if obj.image and not str(obj.image.url).startswith("http"):
+                obj.image = File(open(obj.image.path, 'rb'))
+                obj.save()
+                self.stdout.write(f"Uploaded Trip {obj.title} to Cloudinary")
 
-        for model in models_with_images:
-            # Get all ImageFields in this model
-            image_fields = [f.name for f in model._meta.fields if f.get_internal_type() == 'ImageField']
-
-            for obj in model.objects.all():
-                for field_name in image_fields:
-                    image_field = getattr(obj, field_name)
-                    if image_field:
-                        obj.save()  # triggers Cloudinary upload
-                        self.stdout.write(f"Uploaded {model.__name__}.{field_name} for object {obj.pk}")
-
-        self.stdout.write(self.style.SUCCESS("All images uploaded to Cloudinary!"))
+        for obj in SavedPlace.objects.all():
+            if obj.image and not str(obj.image.url).startswith("http"):
+                obj.image = File(open(obj.image.path, 'rb'))
+                obj.save()
+                self.stdout.write(f"Uploaded SavedPlace {obj.name} to Cloudinary")
